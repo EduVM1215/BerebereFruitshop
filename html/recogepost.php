14bc54ac1a727cac15efc23e2763e8892d8fff25
Kralosxd <potatuncio@gmail.com>
2021-12-01 00:34:47 +0100

    Add files via upload
    conexión con la base de datos

--- html/recogepost.php
+++ html/recogepost.php
@@ -1,3 +1,27 @@
+
+
+
+
+<?php
+require 'conexion.php';
+$conexion->set_charset('utf8'); //establece el conjunto de caracteres en la conexión, para que no haya problema de acentos y ñ de los campos
+
+$Unombre = $_POST['Unombre'];
+$nombre = $_POST['nombre'];
+$apellido1 = $_POST['apellido1'];
+$apellido2 = $_POST['apellido2'];
+$correo = $_POST['correo'];
+$dni = $_POST['dni'];
+$numero = $_POST['numero'];
+$contraseña = $_POST['contraseña'];
+
+$sql = "INSERT INTO `usuario` (`Unombre`, `nombre`, `apellido1`, `apellido2`, `correo`, `dni`, `numero`, `contraseña`) VALUES ('$Unombre', '$nombre', '$apellido1', '$apellido2', '$correo', '$dni', '$numero', '$contraseña')";
+
+$rs = mysqli_query($conexion, $sql);
+?>
+
+
+
 <html>
 <head>
 <meta charset=utf-8 />
@@ -6,7 +30,7 @@
 <body>
 <h1>Datos</h1>
 Tu usuario es <?php echo $_POST['Unombre'] ?> <br>
-Eres <?php echo $_POST['nombre'] & $_POST['apellido1'] & $_POST['apellido2']  ?> <br>
+Eres <?php echo $_POST['nombre'] . ' ' . $_POST['apellido1'] . ' ' . $_POST['apellido2']  ?> <br>
 tu correo es <?php echo $_POST['correo'] ?> <br>
 tu dni es <?php echo $_POST['dni'] ?> <br>
 tu teléfono es <?php echo $_POST['numero'] ?> <br>
